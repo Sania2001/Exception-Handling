b62bd4bd534625aa81b0b7befe2027b69e31f125
Sania2001 <saniagshinde@gmail.com>
2022-03-16 15:18:10 +0530

    .

---
+++ src/com/abstr/Main.java
@@ -0,0 +1,18 @@
+package com.abstr;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                try {
+                    int x=10,y=0,z;
+                    z=x/y;
+                    System.out.println(z);
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e);
+                }
+
+        }
+}
